47940accec9054ef0e59499a2cdcb85ab54948c5
Victor Rinaldi <victorinaldi@ucla.edu>
2022-07-16 11:16:57 +0100

    added contact us page on main page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,13 +2,16 @@
 
 import IntroComponent from "../components/IntroComponent";
 import SBE_Description from "../components/SBE_Description";
+import Contact_gen from "../components/Contact_gen";
 
 import styles from '../styles/css/Home.module.css'
 const Home = () => {
   return (
     <div className={styles.home}>
       <IntroComponent />
-      <SBE_Description/>
+      <SBE_Description />
+      <Contact_gen />
+
     </div>
   );
 }
